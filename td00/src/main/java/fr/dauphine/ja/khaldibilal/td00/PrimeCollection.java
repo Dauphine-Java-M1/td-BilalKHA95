8516fece0f2c56440a0ce65b5c00c471284714f6
khalbi17 <khalbi17@b028-15.crio.dauphine.fr>
2019-09-30 14:16:26 +0200

    intial

---
+++ td00/src/main/java/fr/dauphine/ja/khaldibilal/td00/PrimeCollection.java
@@ -0,0 +1,55 @@
+package fr.dauphine.ja.khaldibilal.td00;
+
+import java.nio.charset.MalformedInputException;
+import java.util.ArrayList;
+import java.util.Random ;
+/**
+ * Hello world!
+ *
+ */
+public class PrimeCollection 
+{
+	private ArrayList<Integer> numbers ; 
+	
+	public static void main(String args[]) {
+		PrimeCollection test = new PrimeCollection() ; 
+		test.initRandom(100, 999);
+		test.printPrimes();
+		
+		
+		
+	}
+	
+	public PrimeCollection() {
+		this.numbers = new ArrayList<Integer>() ; 
+		
+	}
+	
+	public void initRandom(int n , int m ) {
+		for(int i =0 ; i <n ; i++) {
+			Random monAlea = new Random() ; 
+			this.numbers.add(monAlea.nextInt(m+1));
+		}
+	}
+	public boolean isPrime(int p ) {
+		int max = (int)Math.sqrt(p) ; 
+		
+		for (int i= 2 ; i <= max ; i++) {
+			if( (p % i) == 0 ) {
+				return false;
+				}
+			}
+		return true ; 
+	}
+	
+	public void printPrimes() {
+		
+		for (Integer i : this.numbers) {
+			if(this.isPrime(i)) {
+				System.out.println(i) ; 
+				}
+			}
+	}
+	
+    
+}
