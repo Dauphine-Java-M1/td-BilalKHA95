e67d3c72184e57cd053d6150c5c69e7ac70de030
bilal <bilal@192.168.0.26>
2019-11-09 10:15:06 +0100

    MAJ(Question 5-6-7)

--- shapes/src/main/java/fr/dauphine/ja/khaldibilal/shapes/Point.java
+++ shapes/src/main/java/fr/dauphine/ja/khaldibilal/shapes/Point.java
@@ -3,14 +3,24 @@ package fr.dauphine.ja.khaldibilal.shapes;
 public class Point {
 	private int x ; 
 	private int y ; 
+	private static int compteur = 0 ; 
 	
-	
-	public static void main(String ards[]) {
-		Point p = new Point() ; 
-		System.out.println(p.x + "  " + p.y);
-		
+	public Point(int px , int py) {
+		this.x = px ; 
+		this.y = py ; 
+		Point.compteur++ ; 
 		
 	}
 	
+	public int getX() {
+		return this.x ; 
+	}
+	public int getY() {
+		return this.y ; 
+	}
+	public static int getNombrePoint() {
+		return Point.compteur ; 
+		}
+
 
 }
--- shapes/src/test/java/fr/dauphine/ja/khaldibilal/shapes/PointTest.java
+++ shapes/src/test/java/fr/dauphine/ja/khaldibilal/shapes/PointTest.java
@@ -1,5 +1,15 @@
 package fr.dauphine.ja.khaldibilal.shapes;
 
 public class PointTest {
+	
+	public static void main(String args[]) {
+		Point p = new Point(10,10) ; 
+		System.out.println(p.getX()+ "  " + p.getY() + "  " + Point.getNombrePoint());
+		p = new Point(10,10) ; 
+		System.out.println(p.getX()+ "  " + p.getY() + "  " + Point.getNombrePoint());
+		
+		
+	}
+	
 
 }
