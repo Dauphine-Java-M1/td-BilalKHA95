7d1e5527dcd67533adaafbe66d0449cad2fe1705
bilal <bilal@192.168.0.26>
2019-11-09 12:09:45 +0100

    Utilisation d'une linkedList pour la suite de point au lieu du tableau

--- shapes/src/main/java/fr/dauphine/ja/khaldibilal/shapes/LigneBrisee.java
+++ shapes/src/main/java/fr/dauphine/ja/khaldibilal/shapes/LigneBrisee.java
@@ -1,40 +1,30 @@
 package fr.dauphine.ja.khaldibilal.shapes;
 
+import java.util.LinkedList;
 import java.util.Objects;
 
 public class LigneBrisee {
-	private Point m_suitePoint[];
-	private int m_indexLastElement ; 
+	private LinkedList m_suitePoint ; 
+	
 	
 	public LigneBrisee(int nombrePoint) {
-		this.m_suitePoint = new Point[nombrePoint] ;
-		this.m_indexLastElement = 0 ; 
+		this.m_suitePoint = new LinkedList() ;
 	}
 	
 	public void add(Point p) {
 		Objects.requireNonNull(p) ; 
-		if(!(this.m_suitePoint.length == this.m_indexLastElement)) {
-			this.m_suitePoint[this.m_indexLastElement] = p;
-			this.m_indexLastElement++ ;
-			}
+		this.m_suitePoint.add(p) ; 
+		
 		
 	}
 	
-	public int pointCapacity() {
-		return this.m_suitePoint.length ;
-		}
+
 	
 	public int nbPoint() {
-		return this.m_indexLastElement ;
+		return this.m_suitePoint.size() ;
 		}
 	
 	public boolean contains(Point p) {
-		for(Point a : this.m_suitePoint) {
-			if(a.equals(p))return true;
-			}
-		return false ; 
-	}
-	
-			
-
+		return this.m_suitePoint.contains(p);
+		}
 }
