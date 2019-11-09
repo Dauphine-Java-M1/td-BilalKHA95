e790ecbb58a9124136bbd2db3d520cb48e3351ee
bilal <bilal@192.168.0.26>
2019-11-09 12:04:00 +0100

    Exerciice1 avant modification vers linkedList

--- shapes/src/main/java/fr/dauphine/ja/khaldibilal/shapes/LigneBrisee.java
+++ shapes/src/main/java/fr/dauphine/ja/khaldibilal/shapes/LigneBrisee.java
@@ -1,21 +1,38 @@
 package fr.dauphine.ja.khaldibilal.shapes;
 
+import java.util.Objects;
+
 public class LigneBrisee {
 	private Point m_suitePoint[];
+	private int m_indexLastElement ; 
 	
 	public LigneBrisee(int nombrePoint) {
 		this.m_suitePoint = new Point[nombrePoint] ;
+		this.m_indexLastElement = 0 ; 
 	}
 	
 	public void add(Point p) {
-		for(int i=0 ; i < this.m_suitePoint.length ; i++) {
-			if(this.m_suitePoint[i] == null) {
-				this.m_suitePoint[i] = p ;
-				i = this.m_suitePoint.length;
+		Objects.requireNonNull(p) ; 
+		if(!(this.m_suitePoint.length == this.m_indexLastElement)) {
+			this.m_suitePoint[this.m_indexLastElement] = p;
+			this.m_indexLastElement++ ;
 			}
-			
-		}
 		
+	}
+	
+	public int pointCapacity() {
+		return this.m_suitePoint.length ;
+		}
+	
+	public int nbPoint() {
+		return this.m_indexLastElement ;
+		}
+	
+	public boolean contains(Point p) {
+		for(Point a : this.m_suitePoint) {
+			if(a.equals(p))return true;
+			}
+		return false ; 
 	}
 	
 			
