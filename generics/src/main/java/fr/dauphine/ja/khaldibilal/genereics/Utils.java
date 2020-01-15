c6f937c0ae6686408a23c4fd0675f300c5b0e78a
Bilal KHALDI <bilal.khaldi@dauphine.eu>
2020-01-15 09:25:37 +0100

    Exercice 2 - TD4

--- generics/src/main/java/fr/dauphine/ja/khaldibilal/genereics/Utils.java
+++ generics/src/main/java/fr/dauphine/ja/khaldibilal/genereics/Utils.java
@@ -1,5 +1,8 @@
 package fr.dauphine.ja.khaldibilal.genereics;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Utils {
 	public static <T extends Comparable<T>> T myMax(T first, T... tab) {
 		T max = first;
@@ -10,8 +13,23 @@ public class Utils {
 		}
 		return max;
 	}
+	private static void print(List<?> list) {
+		for(Object o:list)
+			System.out.println(o);
+		
+	}
+	
+	private static void printLongueur(List<? extends CharSequence> list) {
+		for(CharSequence a : list) {
+			System.out.println(a.length());
+			
+		}
+	}
 
 	public static void main(String[] args) {
 		System.out.println(myMax(8.6,16.64));
+		List<String> list=Arrays.asList("foo", "toto");
+		print(list);
+
 	}
 }
