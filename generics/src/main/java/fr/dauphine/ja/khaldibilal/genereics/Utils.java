c9e3b3dc1fad950e6ae0074bc3d7d90837fb0262
Bilal KHALDI <bilal.khaldi@dauphine.eu>
2020-01-15 09:58:36 +0100

    Exercice 3 - TD4

--- generics/src/main/java/fr/dauphine/ja/khaldibilal/genereics/Utils.java
+++ generics/src/main/java/fr/dauphine/ja/khaldibilal/genereics/Utils.java
@@ -1,5 +1,6 @@
 package fr.dauphine.ja.khaldibilal.genereics;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,23 +14,39 @@ public class Utils {
 		}
 		return max;
 	}
+
 	private static void print(List<?> list) {
-		for(Object o:list)
+		for (Object o : list)
 			System.out.println(o);
-		
 	}
-	
+
 	private static void printLongueur(List<? extends CharSequence> list) {
-		for(CharSequence a : list) {
+		for (CharSequence a : list) {
 			System.out.println(a.length());
-			
 		}
 	}
 
-	public static void main(String[] args) {
-		System.out.println(myMax(8.6,16.64));
-		List<String> list=Arrays.asList("foo", "toto");
-		print(list);
+	private static <T extends CharSequence> ArrayList<Integer> listLength(List<T> list) {
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+		for (T elem : list) {
+			ret.add(elem.length());
+		}
+		return ret;
+	}
+	
+	private static ArrayList<Integer> listLengthh(List<? extends CharSequence> list){
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+		for (CharSequence elem : list) {
+			ret.add(elem.length());
+		}
+		return ret;
+		
+	}
+	
 
+	public static void main(String[] args) {
+		System.out.println(myMax(8.6, 16.64));
+		List<String> list = Arrays.asList("foo", "toto");
+		print(list);
 	}
 }
